760e31d809ef8b1e258e8119dc3e7448e9c6adc3
Robert-Jung <rjinjung@gmail.com>
2017-06-27 22:22:21 -0700

    User can add ingredient to shopping cart

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -19,4 +19,8 @@ export class ShoppingListComponent implements OnInit {
     console.log(this.ingredients)
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 }
